41dd06e229cf6077a517cab6b8c46e0b3168c7af
victor-akinboro <victorakinboro2003@gmail.com>
2023-01-26 13:28:11 +0700

    next auth secret

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -7,7 +7,8 @@ export const authOptions = {
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET
     })
-  ]
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
 };
 
 export default NextAuth(authOptions)
